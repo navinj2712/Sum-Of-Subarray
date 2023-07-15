b8f7d5ec50b8c6f66ad76c98f05ce45949e60368
navinj2712 <navinj2712@gmail.com>
2023-07-15 22:27:11 +0530

    Initial commit

---
+++ src/SumOfSubarraySum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SumOfSubarraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the size of array : ");
+        int size = scanner.nextInt();
+        System.out.println("Enter the elements of array : ");
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++){
+            array[i] = scanner.nextInt();
+        }
+        int output = findSumOfSubarraySum(array);
+        System.out.println("Output : " + output);
+    }
+
+    private static int findSumOfSubarraySum(int[] array) {
+        int totalSum = 0, subarraySum;
+        for (int i = 0; i < array.length; i++){
+            subarraySum = 0;
+            for (int j = i ; j < array.length; j++){
+                subarraySum += array[j];
+                totalSum += subarraySum;
+            }
+        }
+        return totalSum;
+    }
+}
